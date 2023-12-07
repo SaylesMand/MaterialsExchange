14c5dfba8beb2f77659bbb88fa825ce440970772
Domino <daniel.2003qw@gmail.com>
2023-12-07 08:37:16 +0300

    добавление service для корректной работы Swagger

--- MaterialsExchange/Program.cs
+++ MaterialsExchange/Program.cs
@@ -1,4 +1,6 @@
 using MaterialsExchange.Data;
+using MaterialsExchange.Interfaces;
+using MaterialsExchange.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IMaterialRepository, MaterialRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
