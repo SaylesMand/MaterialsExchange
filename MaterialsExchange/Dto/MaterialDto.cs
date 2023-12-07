5a228cb2c73ac2dfc1a1d60194013408643b8e51
Domino <daniel.2003qw@gmail.com>
2023-12-07 11:33:07 +0300

    Создание Dto (вывод определенных данных) для двух сущностей

---
+++ MaterialsExchange/Dto/MaterialDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MaterialsExchange.Dto
+{
+    public class MaterialDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+    }
+}
---
+++ MaterialsExchange/Dto/SellerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MaterialsExchange.Dto
+{
+    public class SellerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
