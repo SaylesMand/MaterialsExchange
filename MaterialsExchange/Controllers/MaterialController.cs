f771908e7fbb9d9cd9e23fa8f1dbff6812c43d48
Domino <daniel.2003qw@gmail.com>
2023-12-07 08:29:34 +0300

    Создание MaterialController

---
+++ MaterialsExchange/Controllers/MaterialController.cs
@@ -0,0 +1,26 @@
+﻿using MaterialsExchange.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialsExchange.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialController : Controller
+    {
+        private readonly IMaterialRepository _materialRepository;
+        public MaterialController(IMaterialRepository materialRepository)
+        {
+            _materialRepository = materialRepository;
+        }
+        [HttpGet]
+        public IActionResult GetMaterials()
+        {
+            var materials = _materialRepository.GetMaterials();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(materials);
+        }
+    }
+}
