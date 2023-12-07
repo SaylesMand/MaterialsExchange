81a3af75711ff78d567381b50da0971efa7a8d5c
Domino <daniel.2003qw@gmail.com>
2023-12-07 12:29:39 +0300

    [Get]: определение методов в SellerController, используя AutoMapper

--- MaterialsExchange/Controllers/SellerController.cs
+++ MaterialsExchange/Controllers/SellerController.cs
@@ -1,4 +1,6 @@
-﻿using MaterialsExchange.Interfaces;
+﻿using AutoMapper;
+using MaterialsExchange.Dto;
+using MaterialsExchange.Interfaces;
 using MaterialsExchange.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,18 +11,33 @@ namespace MaterialsExchange.Controllers
     public class SellerController : Controller
     {
         private readonly ISellerRepository _sellerRepository;
-        public SellerController(ISellerRepository sellerRepository)
+        private readonly IMapper _mapper;
+        public SellerController(ISellerRepository sellerRepository, IMapper mapper)
         {
             _sellerRepository = sellerRepository;
+            _mapper = mapper;
         }
         [HttpGet]
         public IActionResult GetSellers()
         {
-            var sellers = _sellerRepository.GetSellers();
+            var sellers = _mapper.Map<List<SellerDto>>(_sellerRepository.GetSellers());
 
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             return Ok(sellers);
+        }
+
+        [HttpGet("{sellId}")]
+        public IActionResult GetSeller(int sellId)
+        {
+            if (!_sellerRepository.SellerExists(sellId))
+                return NotFound();
+
+            var seller = _mapper.Map<SellerDto>(_sellerRepository.GetSeller(sellId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(seller);
         }
     }
 }
