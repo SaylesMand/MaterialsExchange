2bd0517cb56d6c9aa3fdac0e53866b4c595df42e
Domino <daniel.2003qw@gmail.com>
2023-12-07 07:46:24 +0300

    Создание AppDbContext для подключения к БД

---
+++ MaterialsExchange/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MaterialsExchange.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaterialsExchange.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Material> Material {  get; set; }
+        public DbSet<Seller> Seller { get; set; }
+    }
+}
