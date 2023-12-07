dc38fd47eafe89c630e72c97d60f7b181c18fd06
Domino <daniel.2003qw@gmail.com>
2023-12-07 11:19:20 +0300

    Добавление Repository и Controller для Seller

---
+++ MaterialsExchange/Controllers/SellerController.cs
@@ -0,0 +1,26 @@
+﻿using MaterialsExchange.Interfaces;
+using MaterialsExchange.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialsExchange.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : Controller
+    {
+        private readonly ISellerRepository _sellerRepository;
+        public SellerController(ISellerRepository sellerRepository)
+        {
+            _sellerRepository = sellerRepository;
+        }
+        [HttpGet]
+        public IActionResult GetSellers()
+        {
+            var sellers = _sellerRepository.GetSellers();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(sellers);
+        }
+    }
+}
---
+++ MaterialsExchange/Repository/SellerRepository.cs
@@ -0,0 +1,20 @@
+﻿using MaterialsExchange.Data;
+using MaterialsExchange.Interfaces;
+using MaterialsExchange.Models;
+
+namespace MaterialsExchange.Repository
+{
+    public class SellerRepository : ISellerRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SellerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Seller> GetSellers()
+        {
+            return _context.Seller.OrderBy(s => s.Id).ToList();
+        }
+    }
+}
